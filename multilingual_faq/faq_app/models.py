3c19d8339994fd22525be4f6000b59753b25266a
abhinavanand-010 <abhinavavi0123@gmail.com>
2025-02-02 09:26:34 +0530

    FAQ model definition

--- multilingual_faq/faq_app/models.py
+++ multilingual_faq/faq_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+
+class FAQ(models.Model):
+    question = models.TextField()
+    answer = RichTextField()
+    question_hi = models.TextField(blank=True, null=True)
+    question_bn = models.TextField(blank=True, null=True)
+    answer_hi = models.TextField(blank=True, null=True)
+    answer_bn = models.TextField(blank=True, null=True)
+
+    def get_translated_question(self, lang='en'):
+        return getattr(self, f'question_{lang}', self.question)
+
+    def get_translated_answer(self, lang='en'):
+        return getattr(self, f'answer_{lang}', self.answer)
+
+    def save(self, *args, **kwargs):
+        from googletrans import Translator
+        translator = Translator()
+        if not self.question_hi:
+            self.question_hi = translator.translate(self.question, dest='hi').text
+        if not self.question_bn:
+            self.question_bn = translator.translate(self.question, dest='bn').text
+        if not self.answer_hi:
+            self.answer_hi = translator.translate(self.answer, dest='hi').text
+        if not self.answer_bn:
+            self.answer_bn = translator.translate(self.answer, dest='bn').text
+        super().save(*args, **kwargs)
